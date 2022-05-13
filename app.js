18ce14dbb7af01f8775808846d2da53b0bf2e311
Johanna Gibbs <johanna.gibbs17@gmail.com>
2022-05-13 16:39:23 +0200

    initial-upload

---
+++ app.js
@@ -0,0 +1,379 @@
+const ohEight = document.getElementById("2008-1");
+const cws = document.getElementById("cws-1");
+const cmyk = document.getElementById("cmyk-1");
+const redact = document.getElementById("redact-1");
+const glory = document.getElementById("glory-1");
+const tsar = document.getElementById("tsar-1");
+const carPrint = document.getElementById("carprint-1");
+const yana = document.getElementById("yana-1");
+const sos = document.getElementById("sos-1");
+const script = document.getElementById("script-1");
+const strelka = document.getElementById("strelka-1");
+const ddg = document.getElementById("ddg-1");
+const abaab = document.getElementById("abaab-1");
+const sans = document.getElementById("index-1");
+const trisha = document.getElementById("trisha-1");
+
+
+function aboutFunction() {
+  var x = document.getElementById("about");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+  }
+}
+
+function content2008Function() {
+  if (ohEight.style.display === "none") {
+    ohEight.style.display = "block";
+    cws.style.display = "none";
+    cmyk.style.display = "none";
+    redact.style.display = "none";
+    tsar.style.display = "none";
+    glory.style.display = "none";
+    carPrint.style.display = "none";
+    yana.style.display = "none";
+    sos.style.display = "none";
+    script.style.display = "none";
+    strelka.style.display = "none";
+    ddg.style.display = "none";
+    abaab.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    ohEight.style.display = "none";
+  }
+}
+
+function contentcwsFunction() {
+  if (cws.style.display === "none") {
+    cws.style.display = "block";
+    ohEight.style.display = "none";
+    cmyk.style.display = "none";
+    redact.style.display = "none";
+    tsar.style.display = "none";
+    glory.style.display = "none";
+    carPrint.style.display = "none";
+    yana.style.display = "none";
+    sos.style.display = "none";
+    script.style.display = "none";
+    strelka.style.display = "none";
+    ddg.style.display = "none";
+    abaab.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    cws.style.display = "none";
+  }
+}
+
+function contentcmykFunction() {
+  if (cws.style.display === "none") {
+    cmyk.style.display = "block";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    redact.style.display = "none";
+    tsar.style.display = "none";
+    glory.style.display = "none";
+    carPrint.style.display = "none";
+    yana.style.display = "none";
+    sos.style.display = "none";
+    script.style.display = "none";
+    strelka.style.display = "none";
+    ddg.style.display = "none";
+    abaab.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    cmyk.style.display = "none";
+  }
+}
+
+function contentredactFunction() {
+  if (redact.style.display === "none") {
+    redact.style.display = "block";
+    cmyk.style.display = "none";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    tsar.style.display = "none";
+    glory.style.display = "none";
+    carPrint.style.display = "none";
+    yana.style.display = "none";
+    sos.style.display = "none";
+    script.style.display = "none";
+    strelka.style.display = "none";
+    ddg.style.display = "none";
+    abaab.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    redact.style.display = "none";
+  }
+}
+
+function contentgloryFunction() {
+  if (glory.style.display === "none") {
+    glory.style.display = "block";
+    redact.style.display = "none";
+    cmyk.style.display = "none";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    tsar.style.display = "none";
+    carPrint.style.display = "none";
+    yana.style.display = "none";
+    sos.style.display = "none";
+    script.style.display = "none";
+    strelka.style.display = "none";
+    ddg.style.display = "none";
+    abaab.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    glory.style.display = "none";
+  }
+}
+
+function contentgloryFunction() {
+  if (glory.style.display === "none") {
+    glory.style.display = "block";
+    redact.style.display = "none";
+    cmyk.style.display = "none";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    tsar.style.display = "none";
+    carPrint.style.display = "none";
+    yana.style.display = "none";
+    sos.style.display = "none";
+    script.style.display = "none";
+    strelka.style.display = "none";
+    ddg.style.display = "none";
+    abaab.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    glory.style.display = "none";
+  }
+}
+
+
+
+function contentcarFunction() {
+  if (carPrint.style.display === "none") {
+    carPrint.style.display = "block";
+    glory.style.display = "none";
+    redact.style.display = "none";
+    cmyk.style.display = "none";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    tsar.style.display = "none";
+    yana.style.display = "none";
+    sos.style.display = "none";
+    script.style.display = "none";
+    strelka.style.display = "none";
+    ddg.style.display = "none";
+    abaab.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    carPrint.style.display = "none";
+  }
+}
+
+function contentyanaFunction() {
+  if (yana.style.display === "none") {
+    yana.style.display = "block";
+    carPrint.style.display = "none";
+    glory.style.display = "none";
+    redact.style.display = "none";
+    cmyk.style.display = "none";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    tsar.style.display = "none";
+    sos.style.display = "none";
+    script.style.display = "none";
+    strelka.style.display = "none";
+    ddg.style.display = "none";
+    abaab.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    yana.style.display = "none";
+  }
+}
+
+function contenttsarFunction() {
+  if (tsar.style.display === "none") {
+    tsar.style.display = "block";
+    carPrint.style.display = "none";
+    glory.style.display = "none";
+    redact.style.display = "none";
+    cmyk.style.display = "none";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    yana.style.display = "none";
+    sos.style.display = "none";
+    script.style.display = "none";
+    strelka.style.display = "none";
+    ddg.style.display = "none";
+    abaab.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    tsar.style.display = "none";
+  }
+}
+
+function contentsosFunction() {
+  if (sos.style.display === "none") {
+    sos.style.display = "block";
+    tsar.style.display = "none";
+    carPrint.style.display = "none";
+    glory.style.display = "none";
+    redact.style.display = "none";
+    cmyk.style.display = "none";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    yana.style.display = "none";
+    script.style.display = "none";
+    strelka.style.display = "none";
+    ddg.style.display = "none";
+    abaab.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    sos.style.display = "none";
+  }
+}
+
+function contentscriptFunction() {
+  if (script.style.display === "none") {
+    script.style.display = "block";
+    sos.style.display = "none";
+    tsar.style.display = "none";
+    carPrint.style.display = "none";
+    glory.style.display = "none";
+    redact.style.display = "none";
+    cmyk.style.display = "none";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    yana.style.display = "none";
+    strelka.style.display = "none";
+    ddg.style.display = "none";
+    abaab.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    script.style.display = "none";
+  }
+}
+
+function contentstrelkaFunction() {
+  if (strelka.style.display === "none") {
+    strelka.style.display = "block";
+    script.style.display = "none";
+    sos.style.display = "none";
+    tsar.style.display = "none";
+    carPrint.style.display = "none";
+    glory.style.display = "none";
+    redact.style.display = "none";
+    cmyk.style.display = "none";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    yana.style.display = "none";
+    ddg.style.display = "none";
+    abaab.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    strelka.style.display = "none";
+  }
+}
+
+function contentddgFunction() {
+  if (ddg.style.display === "none") {
+    ddg.style.display = "block";
+    strelka.style.display = "none";
+    script.style.display = "none";
+    sos.style.display = "none";
+    tsar.style.display = "none";
+    carPrint.style.display = "none";
+    glory.style.display = "none";
+    redact.style.display = "none";
+    cmyk.style.display = "none";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    yana.style.display = "none";
+    abaab.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    ddg.style.display = "none";
+  }
+}
+
+function contentabaabFunction() {
+  if (abaab.style.display === "none") {
+    abaab.style.display = "block";
+    ddg.style.display = "none";
+    strelka.style.display = "none";
+    script.style.display = "none";
+    sos.style.display = "none";
+    tsar.style.display = "none";
+    carPrint.style.display = "none";
+    glory.style.display = "none";
+    redact.style.display = "none";
+    cmyk.style.display = "none";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    yana.style.display = "none";
+    sans.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    abaab.style.display = "none";
+  }
+}
+
+function contentindexFunction() {
+  if (sans.style.display === "none") {
+    sans.style.display = "block";
+    abaab.style.display = "none";
+    ddg.style.display = "none";
+    strelka.style.display = "none";
+    script.style.display = "none";
+    sos.style.display = "none";
+    tsar.style.display = "none";
+    carPrint.style.display = "none";
+    glory.style.display = "none";
+    redact.style.display = "none";
+    cmyk.style.display = "none";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    yana.style.display = "none";
+    trisha.style.display = "none";
+  } else {
+    sans.style.display = "none";
+  }
+}
+
+function contenttrishaFunction() {
+  if (trisha.style.display === "none") {
+    trisha.style.display = "block";
+    sans.style.display = "none";
+    abaab.style.display = "none";
+    ddg.style.display = "none";
+    strelka.style.display = "none";
+    script.style.display = "none";
+    sos.style.display = "none";
+    tsar.style.display = "none";
+    carPrint.style.display = "none";
+    glory.style.display = "none";
+    redact.style.display = "none";
+    cmyk.style.display = "none";
+    cws.style.display = "none";
+    ohEight.style.display = "none";
+    yana.style.display = "none";
+  } else {
+    trisha.style.display = "none";
+  }
+}
